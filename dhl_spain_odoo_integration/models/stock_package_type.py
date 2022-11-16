ea409e814ac790505d1e09515c71b8036875ca11
j_bhautik <vrajatechnologies@gmail.com>
2022-11-16 15:58:03 +0530

    [Add] DHL and GLS Spain

---
+++ dhl_spain_odoo_integration/models/stock_package_type.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from odoo import fields, models
+
+
+class DHLSpainProductPackaging(models.Model):
+    _inherit = 'stock.package.type'
+
+    package_carrier_type = fields.Selection(
+        selection_add=[("dhl_spain", "DHL Spain")])
